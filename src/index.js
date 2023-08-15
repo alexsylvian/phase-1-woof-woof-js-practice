64bd948fef0af038e833b746af6eda96585c9584
alexsylvian <lazeratus1@gmail.com>
2023-08-14 23:24:04 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded'), () => {
+    const dogBar = document.getElementById('dog-bar')
+
+    dogBar.addEventListener('click', () => {
+        console.log('start')
+    })
+}
