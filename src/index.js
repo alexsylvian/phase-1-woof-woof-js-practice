427c3eba164beb9f73740ec792b3ce640ec1c154
alexsylvian <lazeratus1@gmail.com>
2023-08-15 23:57:47 -0400

    added dogbar, doginfo, and button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dogBar = document.getElementById('dog-bar')
+    const dogInfo = document.getElementById('dog-info')
 
-    dogBar.addEventListener('click', () => {
-        console.log('start')
+    addToDogBar()
+
+    function renderPup(pup){
+        dogInfo.innerHTML = `
+        <img src="${pup.image}"</img>
+        <h2>${pup.name}</h2>
+        <button id="goodDogButton">${pup.isGoodDog ? 'Good Dog!' : 'Bad Dog!'}</button>
+        `
+        const goodDogButton = document.getElementById('goodDogButton')
+        goodDogButton.addEventListener('click', () => {
+            toggleGoodness(pup, goodDogButton)
+        })
+    }
+
+    function toggleGoodness(pup, button) {
+            pup.isGoodDog = !pup.isGoodDog
+            button.innerText = pup.isGoodDog ? 'Good Dog!' : 'Bad Dog!';
+            fetch(`http://localhost:3000/pups/${pup.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    isGoodDog:pup.isGoodDog
+                })
+            })
+    }
+    
+
+    function addToDogBar() {
+    fetch('http://localhost:3000/pups')
+    .then(res => res.json())
+    .then(pupData => {
+        pupData.forEach(pup => {
+            const pupSpan = document.createElement('span')
+            pupSpan.innerText = pup.name
+
+            pupSpan.addEventListener('click', () => {
+                console.log(pup)
+                renderPup(pup)
+            })
+
+            dogBar.appendChild(pupSpan)
+        })
     })
+    }
 })
